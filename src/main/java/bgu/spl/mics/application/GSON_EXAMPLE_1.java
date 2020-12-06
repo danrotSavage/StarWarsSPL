c0ae702b18ce65df6fa4cec6b14c160ca06fd765
guyp98 <63144072+guyp98@users.noreply.github.com>
2020-12-06 20:50:47 +0200

    maarichi

---
+++ src/main/java/bgu/spl/mics/application/GSON_EXAMPLE_1.java
@@ -0,0 +1,19 @@
+package main.java.bgu.spl.mics.application;
+
+import main.java.bgu.spl.mics.application.passiveObjects.Attack;
+
+class GSON_EXAMPLE_1
+{
+    /*public GSON_EXAMPLE_1( attack [] att,long R2D2,long Lando,int Ewoks)
+    {
+        this.attacks=att;
+        this.R2D2=R2D2;
+      //  this.Lando=Lando; , long R2D2,long Lando,Long Ewoks
+     //   this.Ewoks=Ewoks;
+    }*/
+
+    Attack[] attacks;
+    long R2D2;
+    long Lando;
+     int Ewoks;
+}
---
+++ src/main/java/bgu/spl/mics/application/OutputJsonClass.java
@@ -0,0 +1,79 @@
+package main.java.bgu.spl.mics.application;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OutputJsonClass {
+    AtomicInteger totalAttacks;
+    long HanSoloFinish=0;
+    long C3POFinish=0;
+    long R2D2Deactivate=0;
+    long LeiaTerminate=0;
+    long HanSoloTerminate=0;
+    long C3POTerminate=0;
+    long R2D2Terminate=0;
+    long LandoTerminate=0;
+
+    public long getHanSoloFinish() {
+        return HanSoloFinish;
+    }
+
+    public void setHanSoloFinish(long hanSoloFinish) {
+        HanSoloFinish = hanSoloFinish;
+    }
+
+    public long getC3POFinish() {
+        return C3POFinish;
+    }
+
+    public void setC3POFinish(long c3POFinish) {
+        C3POFinish = c3POFinish;
+    }
+
+    public long getR2D2Deactivate() {
+        return R2D2Deactivate;
+    }
+
+    public void setR2D2Deactivate(long r2D2Deactivate) {
+        R2D2Deactivate = r2D2Deactivate;
+    }
+
+    public long getLeiaTerminate() {
+        return LeiaTerminate;
+    }
+
+    public void setLeiaTerminate(long leiaTerminate) {
+        LeiaTerminate = leiaTerminate;
+    }
+
+    public long getHanSoloTerminate() {
+        return HanSoloTerminate;
+    }
+
+    public void setHanSoloTerminate(long hanSoloTerminate) {
+        HanSoloTerminate = hanSoloTerminate;
+    }
+
+    public long getC3POTerminate() {
+        return C3POTerminate;
+    }
+
+    public void setC3POTerminate(long c3POTerminate) {
+        C3POTerminate = c3POTerminate;
+    }
+
+    public long getR2D2Terminate() {
+        return R2D2Terminate;
+    }
+
+    public void setR2D2Terminate(long r2D2Terminate) {
+        R2D2Terminate = r2D2Terminate;
+    }
+
+    public long getLandoTerminate() {
+        return LandoTerminate;
+    }
+
+    public void setLandoTerminate(long landoTerminate) {
+        LandoTerminate = landoTerminate;
+    }
+}
